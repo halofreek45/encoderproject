7197f75b25008daccc0cdafc61e2bb91e48dc86b
Propa Panda <halofreek45@gmail.com>
2016-10-13 11:25:47 -0700

    This is my finished Project
    I forgot to COmmit inbetween so sorry

--- encoder.js
+++ encoder.js
@@ -2,6 +2,7 @@
 var textValue;
 var textContent = document.getElementById("textArea");
 var convertedKey;
+var translationContent = document.getElementById("translationArea");
 $("#inputText").keyup(function(e) {
     textValue = document.getElementById("inputText").value; //The input text box
     if ($("input:checked").val() == "caesarCipher") {
@@ -21,7 +22,7 @@ $("#inputText").keyup(function(e) {
         }
     } else if ($("input:checked").val() == "heiroglyphics") {
         if (e.keyCode == 32) {
-            textContent += " "
+            textContent += " ";
             console.log("Added space")
         } else if (e.keyCode == 8) {
             textContent.textContent = textContent.textContent.substr(0, textContent.textContent.length - 1);
@@ -30,6 +31,35 @@ $("#inputText").keyup(function(e) {
             console.log("STUFF" + String.fromCharCode(e.keyCode));
         }
     }
+});
+$("#translationInputText").keyup(function(e)
+{
+  if(e.keyCode == 8)
+  {
+    translationContent.textContent = translationContent.textContent.substr(0, translationContent.textContent.length - 1);
+  }
+  else if(e.keyCode > 65 && e.keyCode <= 90) {
+    translationContent.textContent += String.fromCharCode(e.keyCode - 1);
+  }
+  else if(e.keyCode == 65)
+  {
+    translationContent.textContent += "Z";
+  }
+  else if(e.keyCode == 13) {
+  var decodeMessage = document.getElementById("translationInputText").value;
+  var messageArray = decodeMessage.split("");
+  translationContent.textContent = translationContent.textContent.substr(0, translationContent.textContent.length - 1);
+  for(var i = 0; i < messageArray.length; i++) {
+    var currentCode = messageArray[i].toUpperCase();
+    currentCode = currentCode.charCodeAt(0);
+    console.log(currentCode);
+     if (e.keyCode == 65) {
+      translationArea.textContent += "Z";
+    } else {
+      translationArea.textContent += String.fromCharCode(currentCode - 1);
+    }
+  }
+}
 });
 $("input[type='radio']").click(function() {
     $("input:checked").prop('checked', false);
